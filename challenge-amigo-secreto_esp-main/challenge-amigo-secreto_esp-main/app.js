2209d2cc013df9575d897f9a1d813a6cbce08a4b
Mainnie <espejelmairene@gmail.com>
2025-02-25 12:40:13 -0600

    Add files via upload

---
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -0,0 +1,38 @@
+let amigos = [];
+
+function agregarAmigo() {
+    let input = document.getElementById("amigo");
+    let nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
+
+function actualizarLista() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+
+    amigos.forEach(nombre => {
+        let li = document.createElement("li");
+        li.textContent = nombre;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("No hay amigos en la lista para sortear.");
+        return;
+    }
+
+    let indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    let amigoSorteado = amigos[indiceAleatorio];
+
+    document.getElementById("resultado").innerHTML = `<li>🎉 El amigo secreto es: <strong>${amigoSorteado}</strong>!</li>`;
+}
